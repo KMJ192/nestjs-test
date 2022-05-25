ba2cb04bdc4ca908e9279f30c12476bae8798923
MyeongJunKim <myeongjun222@naver.com>
2022-05-25 23:28:51 +0900

    Mysql연결, 의존성 주입

---
+++ src/user/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './models/user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    // DB 의존성 주입
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async all(): Promise<User[]> {
+    // 모든 유저를 찾아서 반환
+    return await this.userRepository.find();
+  }
+
+  async create(data): Promise<User> {
+    return this.userRepository.save(data);
+  }
+}
