3388c6ba62db03b9b2c086b06f6dad736f2fb40e
MyeongJunKim <myeongjun222@naver.com>
2022-05-26 22:47:41 +0900

    auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { RegisterDto } from './models/register.dto';
 
 @Controller()
 export class AuthController {
   constructor(private userService: UserService) {}
 
   @Post('register')
-  async register(@Body() body) {
-    return this.userService.create(body);
+  async register(@Body() body: RegisterDto) {
+    if (body.password !== body.password_confirm) {
+      throw new BadRequestException('password do not match');
+    }
+
+    const { first_name, last_name, email, password } = body;
+    const hashed = await bcrypt.hash(password, 12);
+    return this.userService.create({
+      first_name,
+      last_name,
+      email,
+      password: hashed,
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  // dto 검증
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
